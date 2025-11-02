b5df8ea804f4834a710beb81da5ecfc251ae2d5b
Mazen <Mazen-Muhamed@outlook.com.>
2025-11-02 10:25:33 +0200

    Implement ESLint & prettier

--- src/app.js
+++ src/app.js
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
-    res.status(200).send("Hello form Acquisitions!");
+  res.status(200).send('Hello form Acquisitions!');
 });
 
 export default app;
